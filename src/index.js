3c1d60a5ac10a0db07826e162834a23778122783
samirm00 <sammoham.be@gmail.com>
2021-02-25 11:55:39 +0100

    add data to courses.json and create a home route

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 "use strict";
+
+"use strict";
+const express = require("express");
+const Joi = require("joi");
+const fs = require("fs");
+const app = express();
+app.use(express.json());
+
+// json file path
+const path = `${__dirname}/../courses.json`;
+
+app.get("/", (req, res) => {
+  res.send(`
+  <div style="text-align:center;margin-top:20px;font-weight:700 ">
+    <h1>Welcome to courses RESTful API</h1><br>
+    <h3>use: /api/courses to access all the courses</h3><br>
+    <h3>use: /api/courses/id to access a specific course with id</h3><br>
+    <h3>use: Postman to GET, POST,  PUT and DELETE course(s)</h3><br> 
+  </div> 
+  `);
+});
