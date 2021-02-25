92bcbbfc259fe5b1d90a339ed7a697a58b7d88ea
samirm00 <sammoham.be@gmail.com>
2021-02-25 14:04:09 +0100

    deploy on Heroku2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 // json file path
-const path = `${__dirname}/../courses.json`;
+const path = `${__dirname}/courses.json`;
 
 app.get("/", (req, res) => {
   res.send(`
